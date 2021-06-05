45a88946103507eefb0feb7dfe0f04ba33d9c4a6
YanSen <yanse@tcd.ie>
2021-06-05 23:52:13 +0100

    index.js modified

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ app.get("/", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 // Server Setup
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, console.log(`Server should never start on port ${PORT}`));
